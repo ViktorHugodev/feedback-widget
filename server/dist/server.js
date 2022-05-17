bdec0380b8b7c98b80bac9a5db9b17ba98190ab0
Victor Hugo <viktorhugo.dev@gmail.com>
2022-05-17 07:59:23 -0300

    ajustes backend para deploy

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,1 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const routes_1 = require("./routes");
+const cors_1 = __importDefault(require("cors"));
+//Esse arquivo fica responsável apenas por ligar o servidor
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(routes_1.routes);
+//Cria a porta em que daria acesso, no caso a porta 3333
+app.listen(3333, () => {
+    console.log('listening onn port 3333');
+});
